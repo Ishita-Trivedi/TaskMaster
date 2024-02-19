1ccf6f8b62bef287940f8e626bd918c2ad41331f
Deependra Kumar <deependrarajpoot178@gmail.com>
2024-02-20 00:26:26 +0530

    update: react-script

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import Input from './components/Input/Input.js';
-import InputList from './components/InputList/InputList.js';
-import './App.css';
+import React, { useState } from "react";
+import Input from "./components/Input/Input.js";
+import InputList from "./components/InputList/InputList.js";
+import "./App.css";
 function App() {
   const [items, setItems] = useState([]);
-  function handleItemInput(item){
-      console.log('Adding item:', item);
-    setItems([...items, {text:item,checked:false}]);
+  function handleItemInput(item) {
+    console.log("Adding item:", item);
+    setItems([...items, { text: item, checked: false }]);
   }
-  const handleDelete=(index)=>{
+  const handleDelete = (index) => {
     // Filter out the item at the specified index
     const updatedItems = items.filter((item, i) => i !== index);
     setItems(updatedItems);
   };
-   const handleToggle = (index) => {
+  const handleToggle = (index) => {
     // Toggle the checked status of the item at the specified index
     const updatedItems = [...items];
     updatedItems[index].checked = !updatedItems[index].checked;
@@ -21,15 +21,20 @@ function App() {
   };
   return (
     <div className="App">
-     <div className="App-header">
-     {/* <h1>To-Day</h1> */}
-     {/* //pass the handleItemInput function to the Input component as a prop */}
-     console.log('started app');
+      <div className="App-header">
+        {/* <h1>To-Day</h1> */}
+        {/* //pass the handleItemInput function to the Input component as a prop */}
+        <div className="text-black">Hello</div>
+        console.log('started app');
         <Input handlerItemInput={handleItemInput} />
       </div>
       <div className="App-content">
-      console.log("fsdbmnfa");
-       <InputList items={items} onDelete={handleDelete} onToggle={handleToggle}/>
+        console.log("fsdbmnfa");
+        <InputList
+          items={items}
+          onDelete={handleDelete}
+          onToggle={handleToggle}
+        />
       </div>
     </div>
   );
